56fd34fb0401e7f6572ae594082b56d5fe3e0373
Emmanuel Rendon <emarendon1301@gmail.com>
2025-10-22 09:22:01 -0500

    Initial commit: Project structure

---
+++ src/main/java/com/tinytask/TinyTask/TinyTaskLearnSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.tinytask.TinyTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyTaskLearnSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyTaskLearnSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tinytask/TinyTask/TinyTaskLearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinytask.TinyTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyTaskLearnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
